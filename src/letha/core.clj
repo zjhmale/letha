4802d5ad486ad468416c2eed54eda52608bae5f3
zjhmale <zjhmale@gmail.com>
2015-07-08 18:03:17 +0800

    add exception handler

--- src/letha/core.clj
+++ src/letha/core.clj
@@ -6,28 +6,33 @@
   (:use [clojure.string :only (join)])
   (:gen-class))
 
+(defn trans
+  [raw]
+  (try
+    (let [keyfrom (env :youdao-keyfrom)
+          key (env :youdao-key)
+          response (client/get (format "http://fanyi.youdao.com/openapi.do?keyfrom=%s&key=%s&type=data&doctype=json&version=1.1&q=%s" keyfrom key raw))
+          domain (blue (get-in response [:cookies "SESSION_FROM_COOKIE" :domain]))
+          body (json/read-str (:body response))
+          basic (get body "basic")
+          showphonetic (fn [cate]
+                         (magenta (str cate (#(str "[ " % " ]") (-> basic (get (str cate "-phonetic")))))))
+          usphonetic (showphonetic "us")
+          ukphonetic (showphonetic "uk")
+          translate (cyan (join " " (get body "translation")))
+          explains (green (join "\n" (map #(str "- " %) (-> basic (get "explains")))))
+          phrases (join "\n" (map-indexed (fn [idx itm]
+                                            (format "%d. %s\n   %s" idx
+                                                    (let [key (-> itm (get "key"))
+                                                          regexraw (re-pattern (str "(?ix) " raw))
+                                                          findraw (re-find regexraw key)]
+                                                      (clojure.string/replace key (re-pattern findraw) (yellow findraw)))
+                                                    (red (join "," (-> itm (get "value")))))) (get body "web")))]
+      (println (format "%s -> %s %s %s ~ %s\n" raw translate usphonetic ukphonetic domain))
+      (println (str explains "\n"))
+      (println (str phrases)))
+    (catch Exception e (println (magenta "something is wrong here")))))
+
 (defn -main
   [& args]
-  (let [raw (nth args 0)
-        keyfrom (env :youdao-keyfrom)
-        key (env :youdao-key)
-        response (client/get (format "http://fanyi.youdao.com/openapi.do?keyfrom=%s&key=%s&type=data&doctype=json&version=1.1&q=%s" keyfrom key raw))
-        domain (blue (get-in response [:cookies "SESSION_FROM_COOKIE" :domain]))
-        body (json/read-str (:body response))
-        basic (get body "basic")
-        showphonetic (fn [cate]
-                       (magenta (str cate (#(str "[ " % " ]") (-> basic (get (str cate "-phonetic")))))))
-        usphonetic (showphonetic "us")
-        ukphonetic (showphonetic "uk")
-        translate (cyan (join " " (get body "translation")))
-        explains (green (join "\n" (map #(str "- " %) (-> basic (get "explains")))))
-        phrases (join "\n" (map-indexed (fn [idx itm]
-                                          (format "%d. %s\n   %s" idx
-                                                  (let [key (-> itm (get "key"))
-                                                        regexraw (re-pattern (str "(?ix) " raw))
-                                                        findraw (re-find regexraw key)]
-                                                    (clojure.string/replace key (re-pattern findraw) (yellow findraw)))
-                                                  (red (join "," (-> itm (get "value")))))) (get body "web")))]
-    (println (format "%s -> %s %s %s ~ %s\n" raw translate usphonetic ukphonetic domain))
-    (println (str explains "\n"))
-    (println (str phrases))))
+  (trans (nth args 0)))
--- test/letha/core_test.clj
+++ test/letha/core_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer :all]
             [letha.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-translate
+  (testing "translate success"
+    (trans "e.g."))
+  (testing "translate failed"
+    (trans "i.e.")))
